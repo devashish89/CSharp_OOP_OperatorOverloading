b3cec3810ada9f119a82d7ff5310e1eef5d8f5c1
Devashish Nigam <devashish.nigam89@gmail.com>
2020-07-29 14:40:45 +0530

    Add project files.

---
+++ CSharp_OOP_OperatorOverloading/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_OOP_OperatorOverloading
+{
+    public class Class1
+    {
+        public string _name="";
+        public short _score=0;
+
+        public Class1()
+        {
+            
+        }
+
+        public Class1(string name="", short score=0)
+        {
+            this._name = name;
+            this._score = score;
+        }
+
+        public static Class1 operator +(Class1 obj1, Class1 obj2)
+        {
+            Class1 obj3 = new Class1();
+            obj3._name = obj1._name + obj2._name;
+            obj3._score = (short)(obj1._score + obj2._score);
+
+            return obj3;
+        }
+    }
+
+
+}
---
+++ ProgrsmMain/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp_OOP_OperatorOverloading;
+
+namespace ProgrsmMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj1 = new Class1("Devashish", 10);
+            Class1 obj2 = new Class1();
+            obj2._name = "Nigam";
+            obj2._score = 20;
+
+            Class1 obj3 = new Class1();
+            obj3 = obj1 + obj2;
+
+            Console.WriteLine(obj3._name+"||"+obj3._score);
+        }
+    }
+}
